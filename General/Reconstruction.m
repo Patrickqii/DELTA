5e6d65529f62fcb2600e76ba9382e2358381aae8
PatrickQi <1449232553@qq.com>
2023-02-22 16:06:05 +0800

    Update Reconstruction.m

--- General/Reconstruction.m
+++ General/Reconstruction.m
@@ -1,23 +1,6 @@
 function [ Specific_Model ] = Reconstruction( Reac_up , Background_Model , Target_Object , Path_Used , Save_Path , Output_File_Name , Output_File_Name_Gene , Output_File_Name_Rxn , Output_File_Name_Met)
-%RECONSTRUCTION 生成特异型代谢网络模型
-%{
-  input params:
-      Reac_up：核心反应集
-      Background_Model：使用的背景代谢网络模型
-      Target_Object：目标函数
-      Path_Used：函数中使用到的函数和文件路径
-      Save_Path：输出文件的路径
-      Output_File_Name：生成的模型的文件名（不含后缀）
-      Output_File_Name_Gene：生成的模型的基因集保存文件名（不含后缀）
-      Output_File_Name_Rxn：生成的模型的反应集保存文件名（不含后缀）
-      Output_File_Name_Met：生成的模型的代谢物集保存文件名（不含后缀）
-  output params:
-      Reac_up：目标代谢反应
+%RECONSTRUCTION :reconstruct context-specific metabolic network model
 
-author:冯昌奇
-create_date:2021-12-08
-last_modify_date:2021-12-17
-%}
 
 if nargin < 9 || ~exist('Output_File_Name_Met','var')
     Output_File_Name_Met = 'Model_Met';
