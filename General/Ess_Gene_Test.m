4f469cb6f66101334d9dfe5055b167f2b5ea5f4d
PatrickQi <1449232553@qq.com>
2023-02-22 16:00:50 +0800

    Update Ess_Gene_Test.m

--- General/Ess_Gene_Test.m
+++ General/Ess_Gene_Test.m
@@ -1,18 +1,5 @@
 function [ ess_gene_name ] = Ess_Gene_Test( Specific_Model , Path_Used , Save_Path , Output_File_Name)
-%ESS_GENE_TEST 使用基因敲除检测必要基因
-%{
-  input params:
-      Specific_Model：特异性代谢网络模型
-      Path_Used：函数中使用到的函数和文件路径
-      Save_Path：输出文件的路径
-      Output_File_Name：生成的模型的文件名（不含后缀）
-  output params:
-      ess_gene_name：必要基因列表
-
-author:冯昌奇
-create_date:2021-12-08
-last_modify_date:2021-12-17
-%}
+%ESS_GENE_TEST %detection of essential genes wtih gene knockout
 
 if nargin < 4 || ~exist('Output_File_Name','var')
     Output_File_Name = 'ess_gene_name';
