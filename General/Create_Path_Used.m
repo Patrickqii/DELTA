a31f2a452ad142d17d822c162b69107daf4d861c
PatrickQi <1449232553@qq.com>
2023-02-22 15:57:11 +0800

    Update Create_Path_Used.m

--- General/Create_Path_Used.m
+++ General/Create_Path_Used.m
@@ -1,15 +1,5 @@
 function [ Path_Used ] = Create_Path_Used()
-%Create_Path_Used 保存函数中使用到的函数和文件路径
-%{
-  input params:
-
-  output params:
-      Path_Used:结构体，保存所有用到的路径
-
-author:冯昌奇
-create_date:2022-01-03
-last_modify_date:2022-01-03
-%}
+%Create_Path_Used %save paths of functions and files thst used in functions
 
 Path_Used=struct();
 Path_Used.map_path='C:\Users\Administrator\Desktop\My_Research\data\Recon3D_301\map_ENSG_to_Entrez.xlsx';
