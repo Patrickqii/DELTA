0fbad0426df016df65908c0220eec46c66823313
PatrickQi <1449232553@qq.com>
2023-02-22 16:29:46 +0800

    Update run.m

--- run.m
+++ run.m
@@ -1,13 +1,13 @@
 
-addpath('C:\Users\Administrator\Desktop\My_Research\code\General');
-load('C:\Users\Administrator\Desktop\My_Research\data\Recon3D_301\Recon3DModel_301.mat');
-%load('C:\Users\Administrator\Desktop\My_Research\data\Recon3D_301\equation.mat');%病毒的生物量合成反应
-load('C:\Users\Administrator\Desktop\biomass_covid\biomass\WT\equation_WT_new.mat');%病毒的生物量合成反应
+addpath('..\General');
+load('..\Data\equation_WT_new.mat');%SARS-CoV-2 VBOF
 Recon3DModel_inf = addReaction( Recon3DModel,'biomass_virus',cell2mat(equation));
 
-covid_path ='C:\Users\Administrator\Desktop\My_Research\data\covid\result\';
+covid_path ='..\Data\result\';
+if exist(covid_path)==0
+    mkdir(covid_path);
+end
 dirs = dir(covid_path);
-%读取所有样本目录
 file_list={};
 for i=1:size(dirs,1)
     isdir=dirs(i).isdir;
@@ -30,4 +30,4 @@ for i = 1:size(file_list,1)
     Main_Process( Recon3DModel_inf , cur_path , 'all_Gene-Entrez_up.txt' , 'biomass_virus' , 0 , 1 , 0 , 1 )
 end
 
-rmpath('C:\Users\Administrator\Desktop\My_Research\code\General');
+rmpath('..\General');
