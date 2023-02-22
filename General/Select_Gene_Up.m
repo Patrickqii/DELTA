9566c11eb1c0310d96807b0c8fb160167b22030c
PatrickQi <1449232553@qq.com>
2023-02-22 16:11:50 +0800

    Update Select_Gene_Up.m

--- General/Select_Gene_Up.m
+++ General/Select_Gene_Up.m
@@ -1,21 +1,5 @@
 function [ Gene_up ] = Select_Gene_Up( Background_Model , Load_Path , Input_File_Name , Save_Path , Output_File_Name , Need_Map , Need_Split )
-%SELECT_GENE_UP 从基因集合中提取出与代谢相关的基因
-%{
-  input params:
-      Background_Model：使用的背景代谢网络模型，该模型中的基因是与代谢相关的基因
-      Load_Path：输入文件的路径
-      Input_File_Name：输入文件的文件名
-      Save_Path：输出文件的路径
-      Output_File_Name：输出文件的文件名（不含后缀）
-      Need_Map：结构体，bool属性表示是否需要将ENSG格式的基因映射到Entrez，1-需要，0-不需要，默认为0。ENSG和Entrez为映射关系，若bool=0则无该属性。
-      Need_Split：基因是否需要切割版本号，1-需要，0-不需要，默认为0
-  output params:
-      Gene_up：目标代谢基因
-
-author:冯昌奇
-create_date:2021-12-08
-last_modify_date:2022-01-03
-%}
+%SELECT_GENE_UP :Extraction of metabolism-related genes from gene collections
 
 if nargin < 7 || ~exist('Need_Split','var')
     Need_Split = 0;
@@ -40,11 +24,11 @@ up_gene = {};
 while(ischar(tline))
     str=tline;
     if Need_Split==1
-        str=regexp( str, '\w+',  'match' );%\w寻找a-z、A-Z、0-9及下划线等单词字符
+        str=regexp( str, '\w+',  'match' );
         str=str(1);
     end
     up_gene=[up_gene;str];
-    tline=fgetl(fid);%读取下一行
+    tline=fgetl(fid);
 end
 fclose(fid);
 genes=Background_Model.genes;
