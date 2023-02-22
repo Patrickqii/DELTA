b5b5082ebc57b61454c573b00ad4a6d978d004c5
PatrickQi <1449232553@qq.com>
2023-02-22 16:05:08 +0800

    Update Private_EvalExpEquation.m

--- General/Private_EvalExpEquation.m
+++ General/Private_EvalExpEquation.m
@@ -1,7 +1,7 @@
 function [res] = Private_EvalExpEquation (exp, x)
 % x can hold 2 values :  1,0 for up-regulated and not
-    if (~( strcmp (exp , '') ) & ~isempty(exp))%如果exp非空且不是空字符串
-       res = eval(exp);%将字符串转为逻辑语句直接出结果
+    if (~( strcmp (exp , '') ) & ~isempty(exp))
+       res = eval(exp);
        
     else
        res = 0;
